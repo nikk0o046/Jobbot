0c29a12eb2e1e74fbd6c682be473b7d54f64a27d
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-06-17 09:53:31 +0300

    main body + requirements

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,32 @@
-from job_test_data import job_data
-print(len(job_data)) 
+from pinecone import init
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from secret_keys import OPENAI_APIKEY, pinecone_apikey, pinecone_environment
+
+test_string = "I have a master's degree in Finance and four years of relevant experience. I am looking for senior level job."
+
+# Initialize Pinecone
+init(
+    api_key=pinecone_apikey,
+    environment=pinecone_environment
+)
+index_name = "jobbotindex"
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_APIKEY)
+
+pinecone_vector_store = Pinecone.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+# Accept user input
+query = test_string
+
+if query:
+# Process the input and display the results
+    docs = pinecone_vector_store.similarity_search(query=query, k=15)
+    sorted_job_list = []
+    for doc in docs:
+        sorted_job_list.append(doc.page_content)
+
+    print(sorted_job_list)
+
