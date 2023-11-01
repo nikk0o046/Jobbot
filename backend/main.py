2604ff430b5688ddfbcf7b776d4b2e86f41daacd
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-11-01 10:36:40 +0200

    Turned it into flask app to integrate with front

--- backend/main.py
+++ backend/main.py
@@ -1,59 +1,51 @@
 import os
-import torch
-from transformers import AutoModel
-from pinecone import init, Index
 import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pinecone import init, Index
 from dotenv import load_dotenv
+from populate_database import get_embedding  # Importing the function from your module
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for the entire app
 
 # Load environment variables
 load_dotenv()
 pinecone_apikey = os.environ['pinecone_apikey']
 pinecone_environment = os.environ['pinecone_environment']
 
-test_string = """Experience: 3 years as an electrician
-Education: Electrician
-Skills: """
-
-# Load the Jina model
-model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-small-en', trust_remote_code=True)
-
-def get_embedding(text):
-    """Generate embeddings using Jina model."""
-    with torch.no_grad():
-        embeddings = model.encode([text])
-    return embeddings[0].tolist()
-
 # Initialize Pinecone
-init(
-    api_key=pinecone_apikey,
-    environment=pinecone_environment
-)
-index_name = "jobbotindex"
+init(api_key=pinecone_apikey, environment=pinecone_environment)
 
-# Load the job listings
-with open('norway_construction_data.json', 'r', encoding='utf-8') as file:
+# Load job listings from the JSON file
+with open('norway_construction_data_no_duplicates.json', 'r', encoding='utf-8') as file:
     job_listings = json.load(file)["content"]
 
 # Create a mapping from UUIDs to job listings
 job_dict = {job["uuid"]: job for job in job_listings}
 
 # Create a Pinecone index instance
-index = Index(index_name)
+index = Index("jobbotindex")
 
-# Accept user input
-query = test_string
 
-if query:
-    # Convert the query to embeddings using the Jina model
-    query_embedding = get_embedding(query)
+@app.route('/function-jobbot', methods=['POST'])
+def search_jobs():
+    data = request.json
+    input_string = data.get('input_string')
+
+    if not input_string:
+        return jsonify({"error": "input_string is required!"}), 400
+
+    # Convert the query to embeddings using the get_embedding function
+    query_embedding = get_embedding(input_string)
     
-    # Process the input and display the results
+    # Process the input and fetch relevant job listings
     docs = index.query(queries=[query_embedding], top_k=15)
-    sorted_job_list = [job_dict[doc.id] for doc in docs.results[0].matches]
+    # Turn returned UUIDs into a list of job summaries which the frontend expects
+    job_summaries = [job_dict[doc.id]['summary'] for doc in docs.results[0].matches]
 
-    print("NEW QUERY!!!!!!!!!!!!!!!!!!!!!")
-    for item in sorted_job_list:
-        print(f"Job uuid: {item['uuid']}")
-        print(item['summary'], '\n')
-        print("---------------------------------")
+    return jsonify(job_summaries), 200
 
+# Run the app on default port 5000
+if __name__ == '__main__':
+    app.run(debug=True)
